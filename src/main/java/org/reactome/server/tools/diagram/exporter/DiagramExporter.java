0cb977eeba1d388880b28d93442662bd29bf05e6
gviteri <gviteri@ebi.ac.uk>
2017-02-10 21:54:29 +0000

    Main class. Generate ppt based on given parameters

--- src/main/java/org/reactome/server/tools/diagram/exporter/DiagramExporter.java
+++ src/main/java/org/reactome/server/tools/diagram/exporter/DiagramExporter.java
@@ -1,89 +1,58 @@
 package org.reactome.server.tools.diagram.exporter;
 
-import org.apache.commons.io.FileUtils;
+import com.martiansoftware.jsap.*;
+import org.reactome.server.tools.diagram.exporter.common.profiles.factory.DiagramJsonDeserializationException;
+import org.reactome.server.tools.diagram.exporter.common.profiles.factory.DiagramJsonNotFoundException;
+import org.reactome.server.tools.diagram.exporter.common.profiles.factory.DiagramProfileException;
 import org.reactome.server.tools.diagram.exporter.pptx.PowerPointExporter;
+import org.reactome.server.tools.diagram.exporter.pptx.exception.LicenseException;
 import org.reactome.server.tools.diagram.exporter.pptx.model.Decorator;
 
 import java.io.File;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Main class for the diagram exporter project
  */
 public class DiagramExporter {
 
-    // TODO Convert this Class to a TEST Class
-    public static void main2(String[] args) throws Exception {
-        // Because the graph-core is not a dependency of this project and won't be, then once the files are exported
-        // we won't create the dbVersion folder. This class is for testing only.
-        for (ColorProfiles colorProfile : ColorProfiles.values()) {
-            for (String stId : getStId()) {
-                File path = new File("/Users/reactome/Reactome/diagram/exporter/" + colorProfile.name().toLowerCase());
-                if (!path.exists()) path.mkdirs();
-                String outputFile = path.getPath() + "/" + stId + ".pptx";
-                PowerPointExporter.export("/Users/reactome/Reactome/diagram/static/" + stId, colorProfile.name().toLowerCase(), outputFile, null);
-            }
-        }
-        System.out.println("Diagrams exported.");
-    }
+    public static void main(String[] args) throws JSAPException, LicenseException, DiagramProfileException, DiagramJsonNotFoundException, DiagramJsonDeserializationException {
+        // Program Arguments -h, -p, -u, -k
+        SimpleJSAP jsap = new SimpleJSAP(DiagramExporter.class.getName(), "Export a given diagram to Power Point",
+                new Parameter[]{
+                        new FlaggedOption("stId",         JSAP.STRING_PARSER, "R-HSA-69620", JSAP.REQUIRED,     'i', "stId",          "Stable Identifier of the diagram"),
+                        new FlaggedOption("colorProfile", JSAP.STRING_PARSER, "Modern",      JSAP.NOT_REQUIRED, 'p', "color profile", "The color profile"),
+                        new FlaggedOption("outputFolder", JSAP.STRING_PARSER, null,          JSAP.REQUIRED,     'o', "output",        "The output folder"),
+                        new FlaggedOption("staticFolder", JSAP.STRING_PARSER, null,          JSAP.REQUIRED,     'j', "static",        "The static json's folder")
+                }
+        );
 
-    // TODO Convert this Class to a TEST Class
-    public static void main3(String[] args) throws Exception {
-        if (args == null || args.length == 0 ) return;
-        String colorProfile = args[0].toLowerCase();
-        for (int i = 1; i < args.length; i++) {
-            String stId = args[i];
-            File path = new File("/Users/reactome/Reactome/diagram/exporter/" + colorProfile);
-            if (!path.exists()) path.mkdirs();
-            String outputFile = path.getPath() + "/" + stId + ".pptx";
-            PowerPointExporter.export("/Users/reactome/Reactome/diagram/static/" + stId, colorProfile, outputFile, null);
+        FlaggedOption flag =  new FlaggedOption("flg", JSAP.LONG_PARSER, null, JSAP.NOT_REQUIRED, 'f', "flag", "Flag nodes");
+        flag.setList(true);
+        flag.setListSeparator(',');
+        jsap.registerParameter(flag);
 
-            //gui's machine
-            FileUtils.copyFile(new File(outputFile), new File("/Volumes/gviteri/exporter/"+stId + ".pptx"));
-        }
+        FlaggedOption selection =  new FlaggedOption("sel", JSAP.LONG_PARSER, null, JSAP.NOT_REQUIRED, 's', "selection", "Select nodes or edges");
+        selection.setList(true);
+        selection.setListSeparator(',');
+        jsap.registerParameter(selection);
 
-        System.out.println("Diagrams exported.");
-    }
+        JSAPResult config = jsap.parse(args);
+        if (jsap.messagePrinted()) System.exit(1);
 
-    // TODO Convert this Class to a TEST Class
-    public static void main(String[] args) throws Exception {
-        if (args == null || args.length == 0 ) return;
-        String colorProfile = args[0].toLowerCase();
-        for (int i = 1; i < args.length; i++) {
-            String stId = args[i];
-            File path = new File("/Users/reactome/Reactome/diagram/exporter/" + colorProfile);
-            if (!path.exists()) path.mkdirs();
-            String outputFile = path.getPath() + "/" + stId + ".pptx";
-            Decorator decorator = new Decorator(Arrays.asList(8852316L,3786256L), Arrays.asList(8852354L,8852316L,182558L,29358L,68374L,113582L,69604L,69591L));
-            PowerPointExporter.export("/Users/reactome/Reactome/diagram/static/" + stId, colorProfile, outputFile, decorator);
-        }
+        String colorProfile = config.getString("colorProfile");
+        String stId = config.getString("stId");
+        File path = new File(config.getString("outputFolder") + colorProfile);
+        if (!path.exists()) path.mkdirs();
+        String outputFile = path.getPath() + "/" + stId + ".pptx";
+        List<Long> flgs = Arrays.stream(config.getLongArray("flg")).boxed().collect(Collectors.toList());
+        List<Long> sels = Arrays.stream(config.getLongArray("sel")).boxed().collect(Collectors.toList());
+        Decorator decorator = new Decorator(flgs, sels);
+        PowerPointExporter.export(config.getString("staticFolder") + stId, colorProfile, outputFile, decorator);
 
         System.out.println("Diagrams exported.");
-    }
 
-    /**
-     * Get a list of Stable Identifier to test.
-     */
-    private static List<String> getStId() {
-        List<String> stdIds = new ArrayList<>();
-//        stdIds.add("R-HSA-169911"); // To start with...
-//        stdIds.add("R-MMU-2990846"); // Activator, simple diagram with long text to be place inside the shape
-//        stdIds.add("R-HSA-2990846"); // Activator, nice diagram layout.
-//        stdIds.add("R-HSA-177929"); // Inhibitor
-//        stdIds.add("R-HSA-69620"); // CDKN1A Gene and CDKN1A mRNA Shape. Links, Activator and Inhibitor
-//        stdIds.add("R-HSA-5205647"); // Mitophagy, multiple Compartments
-//        stdIds.add("R-HSA-1489509"); // Multiple Compartments
-//        stdIds.add("R-HSA-162909"); // Disease
-//        stdIds.add("R-HSA-5603041"); // Crossed and Fade out
-//        stdIds.add("R-HSA-5603029"); // Crossed and Fade out
-//        stdIds.add("R-HSA-1222556"); // Encapsulated Pathway
-        stdIds.add("R-HSA-388396"); // Messy!
-        return stdIds;
-    }
-
-    private enum ColorProfiles {
-        Standard, Modern
     }
 }
