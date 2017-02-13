a6eb016dd3de05957c168f26bd1c6dd220d15e5a
gviteri <gviteri@ebi.ac.uk>
2017-02-13 15:54:41 +0000

    minor changes to be compatible with new export method

--- src/main/java/org/reactome/server/tools/diagram/exporter/common/profiles/service/DiagramService.java
+++ src/main/java/org/reactome/server/tools/diagram/exporter/common/profiles/service/DiagramService.java
@@ -4,7 +4,6 @@ import org.reactome.server.tools.diagram.exporter.common.profiles.factory.Diagra
 import org.reactome.server.tools.diagram.exporter.common.profiles.factory.DiagramJsonNotFoundException;
 import org.reactome.server.tools.diagram.exporter.common.profiles.factory.DiagramProfileException;
 import org.reactome.server.tools.diagram.exporter.pptx.PowerPointExporter;
-import org.reactome.server.tools.diagram.exporter.pptx.exception.LicenseException;
 import org.reactome.server.tools.diagram.exporter.pptx.model.Decorator;
 
 import java.io.File;
@@ -13,24 +12,21 @@ import java.io.File;
  * @author Guilherme S Viteri <gviteri@ebi.ac.uk>
  */
 
+@SuppressWarnings("unused")
 public class DiagramService {
 
     /**
      * Service layer that provides access to the pptx exporter
      * This method won't work in case of invalid software license
      *
-     * @param diagramJsonFullpath full path for the diagram json
+     * @param stId                stable identifier of the diagram
+     * @param diagramJsonFolder   static for the diagram json
      * @param profileName         Color profile name
-     * @param outputPath          output folder + stId.pptx will be written based on the color profile
+     * @param outputFolder        output folder, the given folder + the color profile as a folder.
      * @return the pptx file
-     * @throws DiagramJsonDeserializationException, DiagramProfileException, DiagramJsonNotFoundException, LicenseException
+     * @throws DiagramJsonDeserializationException, DiagramProfileException, DiagramJsonNotFoundException
      */
-    public File exportToPPTX(String diagramJsonFullpath, String profileName, String outputPath, Decorator decorator) throws DiagramJsonDeserializationException, DiagramProfileException, DiagramJsonNotFoundException, LicenseException {
-        PowerPointExporter.export(diagramJsonFullpath, profileName.toLowerCase(), outputPath, decorator);
-        if(decorator.isDecorated()){
-            // add the tmp file extension. We don't save the file if there is flag or selection
-            outputPath = outputPath + Decorator.EXTENSION;
-        }
-        return new File(outputPath);
+    public File exportToPPTX(String stId, String diagramJsonFolder, String profileName, String outputFolder, Decorator decorator) throws DiagramJsonDeserializationException, DiagramProfileException, DiagramJsonNotFoundException {
+        return PowerPointExporter.export(stId, diagramJsonFolder, profileName.toLowerCase(), outputFolder, decorator, "");
     }
 }
