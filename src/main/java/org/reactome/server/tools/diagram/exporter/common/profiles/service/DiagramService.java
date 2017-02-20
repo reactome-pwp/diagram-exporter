fea6c7b3910945f9361fb12ad2122f5d7b35d16d
gviteri <gviteri@ebi.ac.uk>
2017-02-20 16:20:45 +0000

    details in the documentation and new method accepting the license file

--- src/main/java/org/reactome/server/tools/diagram/exporter/common/profiles/service/DiagramService.java
+++ src/main/java/org/reactome/server/tools/diagram/exporter/common/profiles/service/DiagramService.java
@@ -17,16 +17,34 @@ public class DiagramService {
 
     /**
      * Service layer that provides access to the pptx exporter
-     * This method won't work in case of invalid software license
+     * This method assumes a software is embedded in the project, otherwise evaluation will be generated
      *
      * @param stId                stable identifier of the diagram
      * @param diagramJsonFolder   static for the diagram json
      * @param profileName         Color profile name
      * @param outputFolder        output folder, the given folder + the color profile as a folder.
+     * @param decorator           class that holds the flagged and the selected elements into two different lists.
      * @return the pptx file
      * @throws DiagramJsonDeserializationException, DiagramProfileException, DiagramJsonNotFoundException
      */
     public File exportToPPTX(String stId, String diagramJsonFolder, String profileName, String outputFolder, Decorator decorator) throws DiagramJsonDeserializationException, DiagramProfileException, DiagramJsonNotFoundException {
         return PowerPointExporter.export(stId, diagramJsonFolder, profileName.toLowerCase(), outputFolder, decorator, "");
+    }
+
+    /**
+     * Service layer that provides access to the pptx exporter
+     * This method assumes a valid software is provided, otherwise evaluation will be generated
+     *
+     * @param stId                stable identifier of the diagram
+     * @param diagramJsonFolder   static for the diagram json
+     * @param profileName         Color profile name
+     * @param outputFolder        output folder, the given folder + the color profile as a folder.
+     * @param decorator           class that holds the flagged and the selected elements into two different lists.
+     * @param licenseFilePath     a valid Aspose Software License.
+     * @return the pptx file
+     * @throws DiagramJsonDeserializationException, DiagramProfileException, DiagramJsonNotFoundException
+     */
+    public File exportToPPTX(String stId, String diagramJsonFolder, String profileName, String outputFolder, Decorator decorator, String licenseFilePath) throws DiagramJsonDeserializationException, DiagramProfileException, DiagramJsonNotFoundException {
+        return PowerPointExporter.export(stId, diagramJsonFolder, profileName.toLowerCase(), outputFolder, decorator, licenseFilePath);
     }
 }
