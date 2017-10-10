c50863e4dde9d33093bf1a8a8c048de1df70feea
Pascual Lorente <pasculorente@gmail.com>
2017-10-10 15:06:22 +0100

    Fixed the args order and the output value

--- src/main/java/org/reactome/server/tools/diagram/exporter/common/profiles/service/DiagramService.java
+++ src/main/java/org/reactome/server/tools/diagram/exporter/common/profiles/service/DiagramService.java
@@ -1,15 +1,14 @@
 package org.reactome.server.tools.diagram.exporter.common.profiles.service;
 
+import org.reactome.server.tools.diagram.exporter.common.Decorator;
 import org.reactome.server.tools.diagram.exporter.common.profiles.factory.DiagramJsonDeserializationException;
 import org.reactome.server.tools.diagram.exporter.common.profiles.factory.DiagramJsonNotFoundException;
 import org.reactome.server.tools.diagram.exporter.common.profiles.factory.DiagramProfileException;
 import org.reactome.server.tools.diagram.exporter.pptx.PowerPointExporter;
-import org.reactome.server.tools.diagram.exporter.common.Decorator;
 import org.reactome.server.tools.diagram.exporter.raster.RasterExporter;
 
+import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.IOException;
-import java.io.OutputStream;
 
 /**
  * @author Guilherme S Viteri <gviteri@ebi.ac.uk>
@@ -19,55 +18,83 @@ import java.io.OutputStream;
 public class DiagramService {
 
 	/**
-	 * Service layer that provides access to the pptx exporter
-	 * This method assumes a software is embedded in the project, otherwise evaluation will be generated
+	 * Service layer that provides access to the pptx exporter This method
+	 * assumes a software is embedded in the project, otherwise evaluation will
+	 * be generated
 	 *
 	 * @param stId              stable identifier of the diagram
 	 * @param diagramJsonFolder static for the diagram json
 	 * @param profileName       Color profile name
-	 * @param outputFolder      output folder, the given folder + the color profile as a folder.
-	 * @param decorator         class that holds the flagged and the selected elements into two different lists.
+	 * @param outputFolder      output folder, the given folder + the color
+	 *                          profile as a folder.
+	 * @param decorator         class that holds the flagged and the selected
+	 *                          elements into two different lists.
+	 *
 	 * @return the pptx file
-	 * @throws DiagramJsonDeserializationException, DiagramProfileException, DiagramJsonNotFoundException
+	 *
+	 * @throws DiagramJsonDeserializationException, DiagramProfileException,
+	 *                                              DiagramJsonNotFoundException
 	 */
 	public File exportToPPTX(String stId, String diagramJsonFolder, String profileName, String outputFolder, Decorator decorator) throws DiagramJsonDeserializationException, DiagramProfileException, DiagramJsonNotFoundException {
 		return PowerPointExporter.export(stId, diagramJsonFolder, profileName.toLowerCase(), outputFolder, decorator, "");
 	}
 
 	/**
-	 * Service layer that provides access to the pptx exporter
-	 * This method assumes a valid software is provided, otherwise evaluation will be generated
+	 * Service layer that provides access to the pptx exporter This method
+	 * assumes a valid software is provided, otherwise evaluation will be
+	 * generated
 	 *
 	 * @param stId              stable identifier of the diagram
 	 * @param diagramJsonFolder static for the diagram json
 	 * @param profileName       Color profile name
-	 * @param outputFolder      output folder, the given folder + the color profile as a folder.
-	 * @param decorator         class that holds the flagged and the selected elements into two different lists.
+	 * @param outputFolder      output folder, the given folder + the color
+	 *                          profile as a folder.
+	 * @param decorator         class that holds the flagged and the selected
+	 *                          elements into two different lists.
 	 * @param licenseFilePath   a valid Aspose Software License.
+	 *
 	 * @return the pptx file
-	 * @throws DiagramJsonDeserializationException, DiagramProfileException, DiagramJsonNotFoundException
+	 *
+	 * @throws DiagramJsonDeserializationException, DiagramProfileException,
+	 *                                              DiagramJsonNotFoundException
 	 */
 	public File exportToPPTX(String stId, String diagramJsonFolder, String profileName, String outputFolder, Decorator decorator, String licenseFilePath) throws DiagramJsonDeserializationException, DiagramProfileException, DiagramJsonNotFoundException {
 		return PowerPointExporter.export(stId, diagramJsonFolder, profileName.toLowerCase(), outputFolder, decorator, licenseFilePath);
 	}
 
 	/**
-	 * Service layer that provides access to the raster exporter.
+	 * Service layer that provides access to the raster exporter. This service
+	 * outputs the result as a BufferedImage, not to a File.
+	 * <p>
+	 * To save the image to an URL: <code>
+	 * <pre>
+	 * URL url = new URL("http://host.com/");
+	 * HttpUrlConnection connection = (HttpUrlConnection) url.openConnection();
+	 * connection.setDoOutput(true);  // your url must support writing
+	 * OutputStream os = connection.getOutputStream();
+	 * ImageIO.write(image, ext, os);
+	 * </pre>
+	 * </code>
+	 * <p>
+	 * To save to a File <code>
+	 * <pre>
+	 * File file = new File(path, stId + "." + ext);
+	 * ImageIO.write(image, ext, file);
+	 * </pre>
+	 * </code>
 	 *
-	 * @param stId          stable identifier of the diagram
-	 * @param diagramFolder static for the diagram json
-	 * @param profileName   Color profile name
-	 * @param output        image will be written in this OutputStream
-	 * @param decorator     class that holds the flagged and the selected elements into two different lists.
-	 * @param factor        quality of output image. Factor represents the number of pixels per points in the diagram
-	 * @param format        output format: JPEG, PNG, GIF, BMP and WBMP. Case insensitive
+	 * @param stId        stable identifier of the diagram
+	 * @param ext         output format: jpg, jpeg, png or gif. Case
+	 *                    insensitive
+	 * @param factor      quality of output image. Factor represents the number
+	 *                    of pixels per point in the diagram
+	 * @param decorator   flagged and selected elements
+	 * @param profile     Color profile name (modern or standard)
+	 * @param diagramPath static directory for the diagram json
 	 */
-	public void exportToRaster(String stId, String diagramFolder,
-	                           String profileName, OutputStream output,
-	                           Decorator decorator, String format, double factor)
+	public BufferedImage exportToRaster(String stId, String ext, Double factor, Decorator decorator, String profile, String diagramPath)
 			throws DiagramProfileException, DiagramJsonDeserializationException,
-			DiagramJsonNotFoundException, IOException {
-		RasterExporter.export(stId, diagramFolder, profileName.toLowerCase(), decorator, format, output, factor);
-
+			DiagramJsonNotFoundException {
+		return RasterExporter.export(stId, ext, factor, decorator, profile, diagramPath);
 	}
 }
