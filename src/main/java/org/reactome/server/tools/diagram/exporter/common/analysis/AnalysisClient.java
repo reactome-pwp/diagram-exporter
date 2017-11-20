d90c962c82a7721c765f07ecaa11a91a4cb36109
Pascual Lorente <pasculorente@gmail.com>
2017-11-20 11:54:50 +0000

    Added tracing

--- src/main/java/org/reactome/server/tools/diagram/exporter/common/analysis/AnalysisClient.java
+++ src/main/java/org/reactome/server/tools/diagram/exporter/common/analysis/AnalysisClient.java
@@ -10,6 +10,7 @@ import org.reactome.server.tools.diagram.exporter.common.analysis.model.Analysis
 import org.reactome.server.tools.diagram.exporter.common.analysis.model.AnalysisResult;
 import org.reactome.server.tools.diagram.exporter.common.analysis.model.FoundElements;
 import org.reactome.server.tools.diagram.exporter.common.analysis.model.PathwaySummary;
+import org.reactome.server.tools.diagram.exporter.raster.TraceLogger;
 
 import java.io.DataOutputStream;
 import java.io.IOException;
@@ -18,6 +19,7 @@ import java.net.URL;
 import java.net.URLEncoder;
 import java.nio.charset.Charset;
 import java.util.Collection;
+import java.util.Locale;
 
 public class AnalysisClient {
 
@@ -40,26 +42,32 @@ public class AnalysisClient {
 
 	public static FoundElements getFoundElements(String pathway, String token, String resource) throws AnalysisException, AnalysisServerError {
 		try {
+			long start = System.nanoTime();
 			URL url = new URL(SERVER + SERVICE + "/token/" + URLEncoder.encode(token, "UTF8") + "/found/all/" + pathway + "?resource=" + resource);
 			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 			connection.setRequestMethod("GET");
 			connection.setRequestProperty("Response-Type", "application/json");
-
 			switch (connection.getResponseCode()) {
 				case 200:
 					String json = IOUtils.toString(connection.getInputStream(), Charset.defaultCharset());
+					TraceLogger.trace(String.format(Locale.UK,
+							"[analysis service] %10.3f get values (%d chars received)",
+							((System.nanoTime() - start) / 1e6), json.length()));
 					return getObject(FoundElements.class, json);
 				default:
-					String error = IOUtils.toString(connection.getErrorStream(), Charset.defaultCharset());
+					String error = IOUtils.toString(connection.getInputStream(), Charset.defaultCharset());
+					// This is throwing a DeseralizationException
 					throw new AnalysisException(getObject(AnalysisError.class, error));
 			}
 		} catch (IOException | DeserializationException e) {
-			throw new AnalysisServerError(e.getMessage());
+//			throw new AnalysisServerError(e.getMessage());
 		}
+		return null;
 	}
 
 	public static PathwaySummary[] getPathwaysSummary(Collection<String> pathways, String token, String resource) throws AnalysisServerError, AnalysisException {
 		try {
+			final long start = System.nanoTime();
 			URL url = new URL(SERVER + SERVICE + "/token/" + token + "/filter/pathways?resource=" + resource);
 			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 			connection.setRequestMethod("POST");
@@ -73,9 +81,13 @@ public class AnalysisClient {
 			switch (connection.getResponseCode()) {
 				case 200:
 					String json = IOUtils.toString(connection.getInputStream(), Charset.defaultCharset());
+					TraceLogger.trace(String.format(Locale.UK,
+							"[analysis service] %10.3f get subpathways (%d chars sent, %d chars received)",
+							((System.nanoTime() - start) / 1e6),
+							String.join(",", pathways).length(), json.length()));
 					return getObject(PathwaySummary[].class, json);
 				default:
-					String error = IOUtils.toString(connection.getErrorStream(), Charset.defaultCharset());
+					String error = IOUtils.toString(connection.getInputStream(), Charset.defaultCharset());
 					throw new AnalysisException(getObject(AnalysisError.class, error));
 			}
 		} catch (IOException | DeserializationException e) {
@@ -100,7 +112,7 @@ public class AnalysisClient {
 					String json = IOUtils.toString(connection.getInputStream(), Charset.defaultCharset());
 					return getObject(AnalysisResult.class, json);
 				default:
-					String error = IOUtils.toString(connection.getErrorStream(), Charset.defaultCharset());
+					String error = IOUtils.toString(connection.getInputStream(), Charset.defaultCharset());
 					throw new AnalysisException(getObject(AnalysisError.class, error));
 			}
 		} catch (IOException | DeserializationException e) {
@@ -118,7 +130,8 @@ public class AnalysisClient {
 
 	public static AnalysisResult getAnalysisResult(String token) throws AnalysisServerError {
 		try {
-			URL url = new URL(AnalysisClient.SERVER + SERVICE + "/token/" + token);
+			final long start = System.nanoTime();
+			URL url = new URL(AnalysisClient.SERVER + SERVICE + "/token/" + token + "?pageSize=0&page=1");
 			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 			connection.setRequestMethod("GET");
 			connection.setRequestProperty("Content-Type", "text/plain");
@@ -127,9 +140,12 @@ public class AnalysisClient {
 			switch (connection.getResponseCode()) {
 				case 200:
 					String json = IOUtils.toString(connection.getInputStream(), Charset.defaultCharset());
+					TraceLogger.trace(String.format(Locale.UK,
+							"[analysis service] %10.3f get result (%d chars received)",
+							((System.nanoTime() - start) / 1e6), json.length()));
 					return getObject(AnalysisResult.class, json);
 				default:
-					String error = IOUtils.toString(connection.getErrorStream(), Charset.defaultCharset());
+					String error = IOUtils.toString(connection.getInputStream(), Charset.defaultCharset());
 					throw new AnalysisException(getObject(AnalysisError.class, error));
 			}
 		} catch (DeserializationException | AnalysisException | IOException e) {
