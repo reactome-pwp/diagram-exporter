9b18b746c48fb51bf494cded6e7c46bc44f33a03
Pascual Lorente <pasculorente@gmail.com>
2017-11-22 11:28:28 +0000

    Improvements in logo

--- src/main/java/org/reactome/server/tools/diagram/exporter/raster/renderers/layers/ImageLayer.java
+++ src/main/java/org/reactome/server/tools/diagram/exporter/raster/renderers/layers/ImageLayer.java
@@ -1,6 +1,6 @@
 package org.reactome.server.tools.diagram.exporter.raster.renderers.layers;
 
-import org.reactome.server.tools.diagram.data.layout.Coordinate;
+import org.reactome.server.tools.diagram.data.layout.NodeProperties;
 
 import java.awt.*;
 import java.awt.geom.Rectangle2D;
@@ -11,12 +11,28 @@ public class ImageLayer extends CommonLayer {
 
 	private List<ImageLayout> images = new ArrayList<>();
 
+	public void add(Image image, NodeProperties bounds) {
+		images.add(new ImageLayout(image, bounds));
+		super.addShape(new Rectangle2D.Double(bounds.getX(), bounds.getY(), bounds.getWidth(), bounds.getHeight()));
+	}
+
 	@Override
 	public void render(Graphics2D graphics) {
-		images.forEach(imageLayout ->
-				graphics.drawImage(imageLayout.image,
-						imageLayout.coordinate.getX().intValue(),
-						imageLayout.coordinate.getY().intValue(), null));
+		images.forEach(imageLayout -> {
+//			final AffineTransform oldTransform = graphics.getTransform();
+//			final AffineTransform transform = new AffineTransform();
+//			transform.translate(oldTransform.getTranslateX(), transform.getTranslateY());
+//			graphics.setTransform(transform);
+//			graphics.drawImage(imageLayout.image, imageLayout.coordinate.getX().intValue(), imageLayout.coordinate.getY().intValue(), null);
+//			graphics.setTransform(oldTransform);
+			graphics.drawImage(imageLayout.image,
+					imageLayout.bounds.getX().intValue(),
+					imageLayout.bounds.getY().intValue(),
+					imageLayout.bounds.getWidth().intValue(),
+					imageLayout.bounds.getHeight().intValue(),
+					null);
+				}
+		);
 	}
 
 	@Override
@@ -24,21 +40,14 @@ public class ImageLayer extends CommonLayer {
 		images.clear();
 	}
 
-	public void add(Image image, Coordinate coordinate) {
-		images.add(new ImageLayout(image, coordinate));
-		final Shape bounds = new Rectangle2D.Double(coordinate.getX(), coordinate.getY(),
-				image.getWidth(null), image.getHeight(null));
-		super.addShape(bounds);
-	}
-
 	private class ImageLayout {
 
 		private final Image image;
-		private final Coordinate coordinate;
+		private final NodeProperties bounds;
 
-		ImageLayout(Image image, Coordinate coordinate) {
+		ImageLayout(Image image, NodeProperties bounds) {
 			this.image = image;
-			this.coordinate = coordinate;
+			this.bounds = bounds;
 		}
 	}
 }
