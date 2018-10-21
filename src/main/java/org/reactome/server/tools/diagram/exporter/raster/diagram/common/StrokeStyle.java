b4f7191df2ebc97671361eb55b29f79988a373d2
pasculorente <pasculorente@gmail.com>
2018-10-21 09:08:44 +0100

    removed verbosity

--- src/main/java/org/reactome/server/tools/diagram/exporter/raster/diagram/common/StrokeStyle.java
+++ src/main/java/org/reactome/server/tools/diagram/exporter/raster/diagram/common/StrokeStyle.java
@@ -2,55 +2,31 @@ package org.reactome.server.tools.diagram.exporter.raster.diagram.common;
 
 import java.awt.*;
 
+import static java.awt.BasicStroke.CAP_BUTT;
+import static java.awt.BasicStroke.JOIN_MITER;
+
 /**
  * Choose among different stroke widths.
  *
  * @author Lorente-Arencibia, Pascual (pasculorente@gmail.com)
  */
 public enum StrokeStyle {
-	/** width: 8 */
-	FLAG {
-		@Override
-		protected float getWidth() {
-			return 8;
-		}
-	},
-	/** width: 5 */
-	HALO {
-		@Override
-		protected float getWidth() {
-			return 5;
-		}
-	},
-	/** width: 3 */
-	SELECTION {
-		@Override
-		protected float getWidth() {
-			return 3;
-		}
-	},
-	/** width: 1 */
-	SEGMENT {
-		@Override
-		protected float getWidth() {
-			return 1;
-		}
-	},
-	/** width: 2 */
-	BORDER {
-		@Override
-		protected float getWidth() {
-			return 2;
-		}
-	};
 
-	private final Stroke NORMAL = new BasicStroke(getWidth(), BasicStroke.CAP_BUTT, BasicStroke.JOIN_MITER);
-	private final Stroke DASHED = new BasicStroke(getWidth(), BasicStroke.CAP_BUTT, BasicStroke.JOIN_MITER,
-			5f, new float[]{5f, 5f}, 5f);
+	FLAG(8),
+	HALO(5),
+	SELECTION(3),
+	SEGMENT (1),
+	BORDER (2);
 
-	protected abstract float getWidth();
+	private final Stroke NORMAL;
+	private final Stroke DASHED;
+
+	StrokeStyle(float width) {
+		NORMAL = new BasicStroke(width, CAP_BUTT, JOIN_MITER);
+		DASHED = new BasicStroke(width, CAP_BUTT, JOIN_MITER, 5f, new float[]{5f, 5f}, 5f);
+	}
 
 	public final Stroke get(boolean dashed) {
-		return dashed ? DASHED : NORMAL;
+		return dashed ? this.DASHED : NORMAL;
 	}
 }
