990c8fad2c66452e8c5734cffdeb1d6dcaed5b73
Pascual Lorente <pasculorente@gmail.com>
2018-11-01 11:45:51 +0000

    modified some sizing

--- src/main/java/org/reactome/server/tools/diagram/exporter/raster/diagram/renderables/RenderableEntitySet.java
+++ src/main/java/org/reactome/server/tools/diagram/exporter/raster/diagram/renderables/RenderableEntitySet.java
@@ -2,12 +2,10 @@ package org.reactome.server.tools.diagram.exporter.raster.diagram.renderables;
 
 import org.reactome.server.tools.diagram.data.layout.Node;
 import org.reactome.server.tools.diagram.exporter.raster.diagram.common.DiagramData;
-import org.reactome.server.tools.diagram.exporter.raster.diagram.common.FontProperties;
 import org.reactome.server.tools.diagram.exporter.raster.diagram.common.ShapeFactory;
 import org.reactome.server.tools.diagram.exporter.raster.diagram.common.StrokeStyle;
 import org.reactome.server.tools.diagram.exporter.raster.diagram.layers.DiagramCanvas;
 import org.reactome.server.tools.diagram.exporter.raster.diagram.layers.DrawLayer;
-import org.reactome.server.tools.diagram.exporter.raster.diagram.layers.TextLayer;
 import org.reactome.server.tools.diagram.exporter.raster.profiles.ColorProfiles;
 import org.reactome.server.tools.diagram.exporter.raster.profiles.NodeColorSheet;
 
@@ -16,7 +14,7 @@ import java.awt.*;
 public class RenderableEntitySet extends RenderableNode {
 
 
-	private static double SET_PADDING = 4;
+	private static final double SET_PADDING = 4;
 
 	RenderableEntitySet(Node node) {
 		super(node);
@@ -45,17 +43,4 @@ public class RenderableEntitySet extends RenderableNode {
 		layer.add(shape, border, stroke);
 	}
 
-	@Override
-	protected void text(DiagramCanvas canvas, ColorProfiles colorProfiles, DiagramData data, double textSplit) {
-		final TextLayer layer = isFadeOut()
-				? canvas.getFadeOutText()
-				: canvas.getText();
-		final Color color = getTextColor(colorProfiles, data.getAnalysis().getType());
-		layer.add(getNode().getDisplayName(),
-				color,
-				getNode().getProp(),
-				SET_PADDING,
-				textSplit,
-				FontProperties.DEFAULT_FONT);
-	}
 }
