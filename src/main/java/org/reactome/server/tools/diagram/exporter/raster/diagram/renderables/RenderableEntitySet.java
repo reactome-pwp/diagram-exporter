6c4af44b4f1aba1302875b60c8c9e2952efc4095
Pascual Lorente <pasculorente@gmail.com>
2018-11-05 13:28:05 +0000

    Fixed EntitySet text padding

--- src/main/java/org/reactome/server/tools/diagram/exporter/raster/diagram/renderables/RenderableEntitySet.java
+++ src/main/java/org/reactome/server/tools/diagram/exporter/raster/diagram/renderables/RenderableEntitySet.java
@@ -2,10 +2,12 @@ package org.reactome.server.tools.diagram.exporter.raster.diagram.renderables;
 
 import org.reactome.server.tools.diagram.data.layout.Node;
 import org.reactome.server.tools.diagram.exporter.raster.diagram.common.DiagramData;
+import org.reactome.server.tools.diagram.exporter.raster.diagram.common.FontProperties;
 import org.reactome.server.tools.diagram.exporter.raster.diagram.common.ShapeFactory;
 import org.reactome.server.tools.diagram.exporter.raster.diagram.common.StrokeStyle;
 import org.reactome.server.tools.diagram.exporter.raster.diagram.layers.DiagramCanvas;
 import org.reactome.server.tools.diagram.exporter.raster.diagram.layers.DrawLayer;
+import org.reactome.server.tools.diagram.exporter.raster.diagram.layers.TextLayer;
 import org.reactome.server.tools.diagram.exporter.raster.profiles.ColorProfiles;
 import org.reactome.server.tools.diagram.exporter.raster.profiles.NodeColorSheet;
 
@@ -43,4 +45,17 @@ public class RenderableEntitySet extends RenderableNode {
 		layer.add(shape, border, stroke);
 	}
 
+	@Override
+	protected void text(DiagramCanvas canvas, ColorProfiles colorProfiles, DiagramData data, double textSplit) {
+		final TextLayer layer = isFadeOut()
+				? canvas.getFadeOutText()
+				: canvas.getText();
+		final Color color = getTextColor(colorProfiles, data.getAnalysis().getType());
+		layer.add(getNode().getDisplayName(),
+				color,
+				getNode().getProp(),
+				SET_PADDING,
+				textSplit,
+				FontProperties.DEFAULT_FONT);
+	}
 }
